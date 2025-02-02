51d26b3e258fa2ec4d66590b41153020507262f4
Anantshah133 <anantshah130305@gmail.com>
2025-02-02 15:53:53 +0530

    Added different font and also made footer repsonsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <footer className='py-32 my-2 md:mx-10'>
-            <div className='flex flex-wrap mb-5'>
-                <div className='w-6/12'>
+        <footer className='pt-32 pb-10 my-2 md:mx-10'>
+            <div className='flex md:flex-row flex-col flex-wrap mb-5'>
+                <div className='md:w-6/12'>
                     <div className=' pr-10'>
                         <img className='w-52' src={assets.logo} alt="logo" />
-                        <p className='text-gray-600 mt-5'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <p className='text-gray-600 md:text-base text-sm mt-5'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                     </div>
                 </div>
-                <div className='w-3/12'>
-                    <h3 className='text-xl font-semibold mb-5 text-gray-600'>Company</h3>
+                <div className='md:w-3/12'>
+                    <h3 className='text-xl font-semibold md:mb-5 mb-3 mt-10 text-gray-600'>Company</h3>
                     <ul className='text-gray-600 flex flex-col gap-2 text-base'>
                         <li className='hover:text-black cursor-pointer'>
                             <Link to={'/'}>Home</Link>
@@ -29,8 +29,8 @@ const Footer = () => {
                         </li>
                     </ul>
                 </div>
-                <div className='w-3/12'>
-                    <h3 className='text-xl font-semibold mb-10 text-gray-600'>Get In Touch</h3>
+                <div className='md:w-3/12'>
+                    <h3 className='text-xl font-semibold md:mb-5 mb-3 mt-10 text-gray-600'>Get In Touch</h3>
                     <ul className='text-gray-600 flex flex-col gap-2 text-base'>
                         <li className='hover:text-black cursor-pointer'>
                             <a href="tel:+919924590125">
@@ -48,6 +48,7 @@ const Footer = () => {
 
             <hr />
 
+            <p className='mt-5 text-center text-gray-800'>Copyright © {new Date().getFullYear()} Anant Shah - All Right Reserved.</p>
         </footer>
     )
 }
